319b470d3040159c8deb52e50c0f62ca26a26dbf
Mykola Opalenko <mykola.opalenko@gmail.com>
2022-08-07 14:35:45 +0300

    change json

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,44 +1,62 @@
 
-import Player from '@vimeo/player';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// const STORAGE_KEY = 'videoplayer-current-time';
+// let currentTime = 0;
+
+// getPlaybackTime();
+// setTime(currentTime);
+
+// player.on('timeupdate', throttle(savePlaybackTime, 1000));
+
+// function savePlaybackTime({ seconds }) {
+//   localStorage.setItem(STORAGE_KEY, seconds);
+// }
+
+// function getPlaybackTime() {
+//   let dataJson = Number(localStorage.getItem(STORAGE_KEY));
+//   if (dataJson) {
+//     currentTime = dataJson;
+//   }
+// }
+
+// function setTime(time) {
+//   player
+//     .setCurrentTime(time)
+//     .then(function (seconds) {
+//       // seconds = the actual time that the player seeked to
+//     })
+//     .catch(function (error) {
+//       switch (error.name) {
+//         case 'RangeError':
+//           // the time was less than 0 or greater than the video’s duration
+//           break;
+
+//         default:
+//           // some other error occurred
+//           break;
+//       }
+//     });
+// }
+
+
+import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
 
-const STORAGE_KEY = 'videoplayer-current-time';
-let currentTime = 0;
+const player = new Vimeo(iframe);
 
-getPlaybackTime();
-setTime(currentTime);
+player.on('timeupdate', throttle(data, 1000));
 
-player.on('timeupdate', throttle(savePlaybackTime, 1000));
-
-function savePlaybackTime({ seconds }) {
-  localStorage.setItem(STORAGE_KEY, seconds);
+function data({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-function getPlaybackTime() {
-  let dataJson = Number(localStorage.getItem(STORAGE_KEY));
-  if (dataJson) {
-    currentTime = dataJson;
-  }
-}
-
-function setTime(time) {
-  player
-    .setCurrentTime(time)
-    .then(function (seconds) {
-      // seconds = the actual time that the player seeked to
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
-
-        default:
-          // some other error occurred
-          break;
-      }
-    });
-}
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
