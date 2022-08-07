a9d41e8af46438adb5fbe30e084eabd2ed652a4f
Mykola Opalenko <mykola.opalenko@gmail.com>
2022-08-07 14:12:30 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+let currentTime = 0;
+
+getPlaybackTime();
+setTime(currentTime);
+
+player.on('timeupdate', throttle(savePlaybackTime, 1000));
+
+function savePlaybackTime({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+function getPlaybackTime() {
+  let dataJson = Number(localStorage.getItem(STORAGE_KEY));
+  if (dataJson) {
+    currentTime = dataJson;
+  }
+}
+
+function setTime(time) {
+  player
+    .setCurrentTime(time)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
 
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+   email: '',
+   message: '',
+};
+
+const refs = {
+   form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('submit', onSubmitClick);
+refs.form.addEventListener('input', throttle(onFormInput, 1000));
+
+populateForm();
+
+function onSubmitClick(evt) {
+   evt.preventDefault();
+
+   evt.currentTarget.reset();
+   localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(evt) {
+   formData[evt.target.name] = evt.target.value.trim();
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+
+   try {
+      const dataJson = localStorage.getItem(STORAGE_KEY);
+      if (!dataJson) return;
+      formData = JSON.parse(dataJson);
+      for (let key in formData) {
+         refs.form.elements[key].value = formData[key];
+      };
+
+   } catch (error) {
+      console.log(error.name);
+      console.log(error.message);
+   };
+}
